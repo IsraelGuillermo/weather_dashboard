1c4a61a9c698f2ae3a9f6a9e28f52e109e83155a
Israel Guillermo <israelguillermo22@gmail.com>
2020-10-16 23:34:50 -0500

    adding updated logic whic creates a button for city searched. application now displays information of city which is search.

--- script.js
+++ script.js
@@ -1,17 +1,69 @@
 var searchedCities = [];
 
-$("#search").on("click", function () {
-  buildQuery();
-});
-
 function buildQuery() {
   var APIkey = "6384891c94809a3c5a57868fd8ab6033";
+  var $cityName = $("#city-name").val().trim();
+
   var queryURL =
-    "api.openweathermap.org/data/2.5/weather?q=" +
+    "http://api.openweathermap.org/data/2.5/weather?units=imperial&q=" +
     $cityName +
     "&appid=" +
     APIkey;
-  var $cityName = $("#city-name").val().trim();
-  searchedCities.push($cityName);
-  console.log(searchedCities);
+  return queryURL;
 }
+
+$("#search").on("click", function () {
+  event.preventDefault();
+  var queryURL = buildQuery();
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(displayInfo);
+  saveCities();
+  renderButton();
+});
+renderButton();
+
+function saveCities() {
+  var citySearched = $("#city-name").val().trim();
+  var cities = {
+    city: citySearched,
+  };
+  searchedCities.push(cities);
+  localStorage.setItem("searchCities", JSON.stringify(searchedCities));
+}
+function renderButton() {
+  $("#buttons").empty();
+
+  var cities = JSON.parse(localStorage.getItem("searchCities"));
+  searchedCities = cities;
+  if (cities !== null) {
+    searchedCities = cities;
+    for (var i = 0; i < searchedCities.length; i++) {
+      $("<button>")
+        .text(searchedCities[i].city)
+        .addClass("btn btn-primary")
+        .appendTo("#buttons");
+    }
+  }
+}
+function displayInfo(response) {
+  $("#weather-info").empty();
+  $("<h1>").text(response.name).appendTo("#weather-info");
+  $("<p>")
+    .text("Temperatue: " + Math.floor(response.main.temp) + " °F")
+    .appendTo("#weather-info");
+  $("<p>")
+    .text("Humidity: " + response.main.humidity + " %")
+    .appendTo("#weather-info");
+  $("<p>")
+    .text("Wind Speed: " + response.wind.speed + "MPH")
+    .appendTo("#weather-info");
+  $("<p>")
+    .text("UV Index: " + response.wind.speed)
+    .appendTo("#weather-info");
+}
+
+$("btn-primary").on("click", function () {
+  alert("hi");
+});
