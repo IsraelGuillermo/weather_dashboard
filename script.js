8746a6d26d0423d5ee85a7c3eb4c74a332751fb6
Israel Guillermo <israelguillermo22@gmail.com>
2020-10-18 23:45:50 -0500

    updating js logic which sets up functionality for 5 day forecast. also updated html and added styling

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ function buildQuery() {
     APIkey;
   return queryURL;
 }
+function buildForecastquery() {
+  var $cityName = $("#city-name").val().trim();
+  var APIkey = "6384891c94809a3c5a57868fd8ab6033";
+
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" +
+    $cityName +
+    "&appid=" +
+    APIkey;
+  return queryURL;
+}
 
 $("#search").on("click", function () {
   event.preventDefault();
@@ -20,7 +31,34 @@ $("#search").on("click", function () {
   }).then(displayInfo);
   saveCities();
   renderButton();
+  var queryURL2 = buildForecastquery();
+  $.ajax({
+    url: queryURL2,
+    method: "GET",
+  }).then(generateForecast);
 });
+
+function generateForecast(response) {
+  $("#forecast").empty();
+  var list = response.list;
+  for (var i = 0; i < list.length; i++) {
+    if (i % 8 === 0) {
+      var div = $("<div>").addClass("card float-left ml-3 my-2 rounded pl-2");
+      var h5 = $("<h5>").addClass("card-title");
+      var p = $("<p>").addClass("card-text");
+      var p1 = $("<p>").addClass("card-text");
+      h5.text(list[i].dt_txt).appendTo(div);
+      p.text(
+        "Temperature: " + Math.floor(list[i].main.temp_min) + " °F"
+      ).appendTo(div);
+      p1.text("Humidity: " + Math.floor(list[i].main.humidity) + " %").appendTo(
+        div
+      );
+      div.appendTo("#forecast");
+    }
+  }
+}
+
 renderButton();
 
 function saveCities() {
@@ -41,28 +79,51 @@ function renderButton() {
     for (var i = 0; i < searchedCities.length; i++) {
       $("<button>")
         .text(searchedCities[i].city)
-        .addClass("btn btn-primary")
+        .addClass("btn btn-primary mb-2")
         .appendTo("#buttons");
     }
   }
 }
 function displayInfo(response) {
-  $("#weather-info").empty();
-  $("<h1>").text(response.name).appendTo("#weather-info");
+  var weatherDiv = $("<div>").addClass("weatherInfo rounded pl-2");
+  $("#main").empty();
+
+  $("<h1>").text(response.name).appendTo(weatherDiv);
   $("<p>")
     .text("Temperatue: " + Math.floor(response.main.temp) + " °F")
-    .appendTo("#weather-info");
+    .appendTo(weatherDiv);
   $("<p>")
     .text("Humidity: " + response.main.humidity + " %")
-    .appendTo("#weather-info");
+    .appendTo(weatherDiv);
   $("<p>")
     .text("Wind Speed: " + response.wind.speed + "MPH")
-    .appendTo("#weather-info");
+    .appendTo(weatherDiv);
   $("<p>")
     .text("UV Index: " + response.wind.speed)
-    .appendTo("#weather-info");
+    .appendTo(weatherDiv);
+  weatherDiv.appendTo("#main");
 }
 
-// $("btn-primary").on("click", function () {
-//   alert("hi");
-// });
+$(".btn-primary").on("click", function () {
+  var APIkey = "6384891c94809a3c5a57868fd8ab6033";
+  var $cityName = $(this).text();
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/weather?units=imperial&q=" +
+    $cityName +
+    "&appid=" +
+    APIkey;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(displayInfo);
+  var queryURL2 =
+    "http://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" +
+    $cityName +
+    "&appid=" +
+    APIkey;
+  $.ajax({
+    url: queryURL2,
+    method: "GET",
+  }).then(generateForecast);
+});
