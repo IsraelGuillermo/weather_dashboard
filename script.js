beaf14458942612ca5f257e1c68e2e0fd7e0ec1c
Israel Guillermo <israelguillermo22@gmail.com>
2020-10-22 16:19:27 -0500

    updating logic to display img and UV index. also enhanced colors and display.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+// Empty array to add cities searched
 var searchedCities = [];
+var moment = moment();
+var today = moment.format("dddd Do MMMM, YYYY");
+var APIkey = "6384891c94809a3c5a57868fd8ab6033";
 
+// This function builds the URL query for current weather
 function buildQuery() {
-  var APIkey = "6384891c94809a3c5a57868fd8ab6033";
   var $cityName = $("#city-name").val().trim();
   var queryURL =
     "http://api.openweathermap.org/data/2.5/weather?units=imperial&q=" +
@@ -10,9 +14,9 @@ function buildQuery() {
     APIkey;
   return queryURL;
 }
+// This function builds URL query for forecast information
 function buildForecastquery() {
   var $cityName = $("#city-name").val().trim();
-  var APIkey = "6384891c94809a3c5a57868fd8ab6033";
 
   var queryURL =
     "http://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" +
@@ -21,7 +25,7 @@ function buildForecastquery() {
     APIkey;
   return queryURL;
 }
-
+// When search is done, this function gets information back from openweather API
 $("#search").on("click", function () {
   event.preventDefault();
   var queryURL = buildQuery();
@@ -38,29 +42,42 @@ $("#search").on("click", function () {
   }).then(generateForecast);
 });
 
+// This function generates the 5 day forecast. Creates divs and appends to forecast div
 function generateForecast(response) {
   $("#forecast").empty();
+  $("<h1>").text("5-Day Forecast:").addClass("mx-3").appendTo("#forecast");
   var list = response.list;
+
   for (var i = 0; i < list.length; i++) {
     if (i % 8 === 0) {
-      var div = $("<div>").addClass("card float-left ml-3 my-2 rounded pl-2");
+      function generateImg(response) {
+        var icon = response.list[i].weather[0].icon;
+        var imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        return imageURL;
+      }
+      var imgUrl = generateImg(response);
+
+      var div = $("<div>").addClass("card float-left mx-4 my-2 rounded");
       var h5 = $("<h5>").addClass("card-title");
       var p = $("<p>").addClass("card-text");
       var p1 = $("<p>").addClass("card-text");
-      h5.text(list[i].dt_txt).appendTo(div);
+      h5.text(new Date(list[i].dt_txt).toLocaleDateString()).appendTo(div);
       p.text(
         "Temperature: " + Math.floor(list[i].main.temp_min) + " °F"
       ).appendTo(div);
       p1.text("Humidity: " + Math.floor(list[i].main.humidity) + " %").appendTo(
         div
       );
+      $("<img>").attr("src", imgUrl).appendTo(div);
+
       div.appendTo("#forecast");
     }
   }
 }
-
+// Calling this function allows system to generate buttons for cities which were searched
 renderButton();
 
+// Function saves searched cities to local storage
 function saveCities() {
   var citySearched = $("#city-name").val().trim();
   var cities = {
@@ -69,6 +86,8 @@ function saveCities() {
   searchedCities.push(cities);
   localStorage.setItem("searchCities", JSON.stringify(searchedCities));
 }
+
+// This function createds a button for every city that has been searched
 function renderButton() {
   $("#buttons").empty();
 
@@ -80,30 +99,76 @@ function renderButton() {
       $("<button>")
         .text(searchedCities[i].city)
         .addClass("btn btn-primary mb-2")
-        .appendTo("#buttons");
+        .prependTo("#buttons");
     }
   }
 }
-function displayInfo(response) {
-  var weatherDiv = $("<div>").addClass("weatherInfo rounded pl-2");
-  $("#main").empty();
 
-  $("<h1>").text(response.name).appendTo(weatherDiv);
+// This function generates original current data information for city which is searched
+function displayInfo(response) {
+  $("#main").empty();
+  function generateImg(response) {
+    var icon = response.weather[0].icon;
+    var imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    return imageURL;
+  }
+  var imgUrl = generateImg(response);
+  $("#main").addClass("weatherInfo rounded my-4 px-4");
+
+  $("<h1>")
+    .text(response.name + " " + today)
+    .appendTo("#main");
+  $("<img>").attr("src", imgUrl).appendTo("#main");
+
   $("<p>")
     .text("Temperatue: " + Math.floor(response.main.temp) + " °F")
-    .appendTo(weatherDiv);
+    .appendTo("#main");
   $("<p>")
     .text("Humidity: " + response.main.humidity + " %")
-    .appendTo(weatherDiv);
+    .appendTo("#main");
   $("<p>")
     .text("Wind Speed: " + response.wind.speed + "MPH")
-    .appendTo(weatherDiv);
-  $("<p>")
-    .text("UV Index: " + response.wind.speed)
-    .appendTo(weatherDiv);
-  weatherDiv.appendTo("#main");
+    .appendTo("#main");
+
+  UV(response.coord.lat, response.coord.lon);
 }
 
+// Function which gets UV index and displays it on main, also displays background color based on level of UV index
+
+function UV(lat, lon) {
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    APIkey;
+  var uvIndex;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    uvIndex = response.value;
+    if (uvIndex < 2) {
+      $("<p>")
+        .text("UV Index: " + uvIndex)
+        .addClass("weatherInfo green")
+        .appendTo("#main");
+    } else if (uvIndex < 8) {
+      $("<p>")
+        .text("UV Index: " + uvIndex)
+        .addClass("weatherInfo orange")
+        .appendTo("#main");
+    } else {
+      $("<p>")
+        .text("UV Index: " + uvIndex)
+        .addClass("weatherInfo red")
+        .appendTo("#main");
+    }
+  });
+  return uvIndex;
+}
+// This function targets buttons which were created when a city was searched. It displays current and forecast weather information
 $(".btn-primary").on("click", function () {
   var APIkey = "6384891c94809a3c5a57868fd8ab6033";
   var $cityName = $(this).text();
